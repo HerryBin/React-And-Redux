2d07524fc3ac52246b7be075deb78eace31118c4
herryBin <xianrongbin1989@126.com>
2017-06-27 22:16:43 +0800

    新增样式，数据源js

---
+++ Student-List/data/data.js
@@ -0,0 +1,54 @@
+function getListData() {
+    return [
+        {
+            id: '01',
+            name: '刘德华',
+            sex: 'M',
+            age: '15',
+            gradeLevel: '1'
+        },
+        {
+            id: '02',
+            name: '黎明',
+            sex: 'M',
+            age: '12',
+            gradeLevel: '5'
+        },
+        {
+            id: '03',
+            name: '张曼玉',
+            sex: 'F',
+            age: '8',
+            gradeLevel: '6'
+        },
+        {
+            id: '04',
+            name: '谢霆锋',
+            sex: 'M',
+            age: '10',
+            gradeLevel: '4'
+        },
+        {
+            id: '05',
+            name: '张白芷',
+            sex: 'F',
+            age: '7',
+            gradeLevel: '2'
+        },
+        {
+            id: '06',
+            name: '陈旭东',
+            sex: 'M',
+            age: '12',
+            gradeLevel: '6'
+        },
+        {
+            id: '07',
+            name: '郑成',
+            sex: 'M',
+            age: '14',
+            gradeLevel: '7'
+        }
+    ]
+
+}
